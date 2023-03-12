298edf587c3927cdf44dbfb79e655cb372e8a043
Alona Ihnatchenko <olena1366@gmail.com>
2023-03-12 22:01:31 +0200

    delete app styled

--- src/components/App/App.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  height: 100vh;
-  padding-top: 60px;
-  padding-left: 100px;
-  font-size: 40px;
-  color: #010101;
-`;
